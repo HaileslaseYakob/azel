a5bc920e3be1ff5cfcbc927a09aa5892cbc5cdca
Haile Yakob <haileyakob@gmail.com>
2020-05-22 13:18:10 -0700

    Update salesforecast.py

--- forecast/models/salesforecast.py
+++ forecast/models/salesforecast.py
@@ -184,7 +184,7 @@ class SalesforecastProductsItems(models.Model):
 
     item_unit_price = fields.Float(
         'Unit Price',
-        related='item_id.standard_price',
+        related='item_id.list_price',
         readonly=False, store=True)
 
     item_total = fields.Float(compute='_compute_total', string='Total',store=True)
@@ -226,7 +226,7 @@ class SalesforecastProductItems(models.Model):
 
     item_unit_price = fields.Float(
         'Unit Price',
-        related='item_id.standard_price',
+        related='item_id.list_price',
         readonly=False, store=True)
 
    
